4cd976a65889d261ce0287cb0da16dc201061fd9
Simon Brown <simon@structurizr.com>
2023-02-04 12:51:51 +0000

    Added home page controller and tests.

---
+++ src/main/java/com/structurizr/onpremises/web/home/HomePageController.java
@@ -0,0 +1,64 @@
+package com.structurizr.onpremises.web.home;
+
+import com.structurizr.onpremises.component.workspace.WorkspaceMetaData;
+import com.structurizr.onpremises.util.HtmlUtils;
+import com.structurizr.onpremises.web.AbstractController;
+import com.structurizr.util.StringUtils;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+@Controller
+public class HomePageController extends AbstractController {
+
+    private static final String SORT_DATE = "date";
+    private static final String SORT_NAME = "name";
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String showHomePage(@RequestParam(required = false) String sort, ModelMap model) {
+        if (isAuthenticated()) {
+            return showDashboardPage(sort, model);
+        } else {
+            model.addAttribute("urlPrefix", "/share");
+            return show(model, sort);
+        }
+    }
+
+    @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
+    @PreAuthorize("isAuthenticated()")
+    public String showDashboardPage(@RequestParam(required = false) String sort, ModelMap model) {
+        model.addAttribute("urlPrefix", "/workspace");
+        return show(model, sort);
+    }
+
+    private String show(ModelMap model, String sort) {
+        List<WorkspaceMetaData> workspaces = new ArrayList<>(getWorkspaces());
+
+        sort = HtmlUtils.filterHtml(sort);
+        if (!StringUtils.isNullOrEmpty(sort) && sort.trim().equals(SORT_DATE)) {
+            Collections.sort(workspaces, (wmd1, wmd2) -> wmd2.getLastModifiedDate().compareTo(wmd1.getLastModifiedDate()));
+
+            sort = SORT_DATE;
+        } else {
+            Collections.sort(workspaces, Comparator.comparing(wmd -> wmd.getName().toLowerCase()));
+
+            sort = SORT_NAME;
+        }
+
+        model.addAttribute("workspaces", workspaces);
+        model.addAttribute("numberOfWorkspaces", workspaces.size());
+        model.addAttribute("sort", sort);
+        addCommonAttributes(model, "", true);
+
+        return "home";
+    }
+
+}
---
+++ src/test/java/com/structurizr/onpremises/web/home/HomePageControllerTests.java
@@ -0,0 +1,81 @@
+package com.structurizr.onpremises.web.home;
+
+import com.structurizr.onpremises.component.workspace.WorkspaceMetaData;
+import com.structurizr.onpremises.web.MockWorkspaceComponent;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.ui.ModelMap;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static com.structurizr.onpremises.web.TestUtils.clearUser;
+import static com.structurizr.onpremises.web.TestUtils.setUser;
+import static org.junit.Assert.*;
+
+public class HomePageControllerTests {
+
+    private HomePageController controller;
+    private MockWorkspaceComponent workspaceComponent;
+    private ModelMap model;
+
+    @Before
+    public void setUp() {
+        controller = new HomePageController();
+
+        model = new ModelMap();
+    }
+
+    @Test
+    public void showHomePage_WhenUnauthenticated() {
+        WorkspaceMetaData workspace1 = new WorkspaceMetaData(1); // private workspace
+        workspace1.addWriteUser("user");
+        WorkspaceMetaData workspace2 = new WorkspaceMetaData(2); // open workspace
+
+        workspaceComponent = new com.structurizr.onpremises.web.MockWorkspaceComponent() {
+            @Override
+            public Collection<WorkspaceMetaData> getWorkspaces() {
+                return Arrays.asList(
+                        workspace1, workspace2
+                );
+            }
+        };
+        controller.setWorkspaceComponent(workspaceComponent);
+        clearUser();
+
+        String result = controller.showHomePage("", model);
+
+        assertEquals(1, model.getAttribute("numberOfWorkspaces"));
+        assertFalse(((Collection)model.getAttribute("workspaces")).contains(workspace1)); // private workspace
+        assertTrue(((Collection)model.getAttribute("workspaces")).contains(workspace2)); // open workspace
+        assertEquals("/share", model.getAttribute("urlPrefix"));
+        assertEquals("home", result);
+    }
+
+    @Test
+    public void showHomePage_WhenAuthenticated() {
+        WorkspaceMetaData workspace1 = new WorkspaceMetaData(1); // private workspace
+        workspace1.addWriteUser("user");
+        WorkspaceMetaData workspace2 = new WorkspaceMetaData(2); // open workspace
+
+        workspaceComponent = new com.structurizr.onpremises.web.MockWorkspaceComponent() {
+            @Override
+            public Collection<WorkspaceMetaData> getWorkspaces() {
+                return Arrays.asList(
+                        workspace1, workspace2
+                );
+            }
+        };
+        controller.setWorkspaceComponent(workspaceComponent);
+        setUser("user");
+
+        String result = controller.showHomePage("", model);
+
+        assertEquals(2, model.getAttribute("numberOfWorkspaces"));
+        assertTrue(((Collection)model.getAttribute("workspaces")).contains(workspace1)); // private workspace
+        assertTrue(((Collection)model.getAttribute("workspaces")).contains(workspace2)); // open workspace
+        assertEquals("/workspace", model.getAttribute("urlPrefix"));
+        assertEquals("home", result);
+    }
+
+}
