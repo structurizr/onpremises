dcfc51f9dce1be4578501a6716ac490102030899
Simon Brown <simon@structurizr.com>
2023-02-04 14:35:03 +0000

    Workspace meta data.

---
+++ src/componentTest/java/com/structurizr/onpremises/component/workspace/WorkspaceMetaDataTests.java
@@ -0,0 +1,233 @@
+package com.structurizr.onpremises.component.workspace;
+
+import com.structurizr.onpremises.domain.AuthenticationMethod;
+import com.structurizr.onpremises.domain.User;
+import com.structurizr.onpremises.util.DateUtils;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class WorkspaceMetaDataTests {
+
+    @Test
+    public void sharingToken() {
+        WorkspaceMetaData workspace = new WorkspaceMetaData(1);
+        assertEquals("", workspace.getSharingToken());
+        assertFalse(workspace.isShareable());
+
+        workspace.setSharingToken("12345678901234567890");
+        assertEquals("12345678901234567890", workspace.getSharingToken());
+        assertEquals("123456...", workspace.getSharingTokenTruncated());
+        assertTrue(workspace.isShareable());
+    }
+
+    @Test
+    public void isOpen_ReturnsTrue_WhenTheWorkspaceIsOpen() {
+        WorkspaceMetaData workspace = new WorkspaceMetaData(1);
+        assertTrue(workspace.isOpen());
+    }
+
+    @Test
+    public void isOpen_ReturnsFalse_WhenTheWorkspaceHasAReadOnlyUser() {
+        WorkspaceMetaData workspace = new WorkspaceMetaData(1);
+        workspace.addReadUser("user");
+        assertFalse(workspace.isOpen());
+    }
+
+    @Test
+    public void isOpen_ReturnsFalse_WhenTheWorkspaceHasAReadWriteUser() {
+        WorkspaceMetaData workspace = new WorkspaceMetaData(1);
+        workspace.addWriteUser("user");
+        assertFalse(workspace.isOpen());
+    }
+
+    @Test
+    public void isReadUser_ReturnsFalse_WhenTheUserIsNotAReadUser() {
+        WorkspaceMetaData workspace = new WorkspaceMetaData(1);
+        workspace.addReadUser("simon");
+
+        User user = new User("user", new HashSet<>(), null);
+        assertFalse(workspace.isReadUser(user));
+    }
+
+    @Test
+    public void isReadUser_ReturnsTrue_WhenTheUserIsAReadUser() {
+        WorkspaceMetaData workspace = new WorkspaceMetaData(1);
+        workspace.addReadUser("user");
+
+        User user = new User("user", new HashSet<>(), null);
+        assertTrue(workspace.isReadUser(user));
+    }
+
+    @Test
+    public void isReadUser_ReturnsFalse_WhenTheUserRoleIsNotAReadUser() {
+        WorkspaceMetaData workspace = new WorkspaceMetaData(1);
+        workspace.addReadUser("role1");
+
+        User user = new User("user", new HashSet<>(List.of("role2")), AuthenticationMethod.LOCAL);
+        assertFalse(workspace.isReadUser(user));
+    }
+
+    @Test
+    public void isReadUser_ReturnsTrue_WhenTheUserRoleIsAReadUser() {
+        WorkspaceMetaData workspace = new WorkspaceMetaData(1);
+        workspace.addReadUser("role1");
+
+        User user = new User("bob", new HashSet<>(List.of("role1", "role2")), AuthenticationMethod.LOCAL);
+        assertTrue(workspace.isReadUser(user));
+    }
+
+    @Test
+    public void isWriteUser_ReturnsFalse_WhenTheUserIsNotAWriteUser() {
+        WorkspaceMetaData workspace = new WorkspaceMetaData(1);
+        workspace.addWriteUser("simon");
+
+        User user = new User("user", new HashSet<>(), null);
+        assertFalse(workspace.isWriteUser(user));
+    }
+
+    @Test
+    public void isWriteUser_ReturnsTrue_WhenTheUserIsAWriteUser() {
+        WorkspaceMetaData workspace = new WorkspaceMetaData(1);
+        workspace.addWriteUser("user");
+
+        User user = new User("user", new HashSet<>(), null);
+        assertTrue(workspace.isWriteUser(user));
+    }
+
+    @Test
+    public void test_isWriteUser_ReturnsFalse_WhenTheUserRoleIsNotAWriteUser() {
+        WorkspaceMetaData workspace = new WorkspaceMetaData(1);
+        workspace.addWriteUser("role1");
+
+        User user = new User("user", new HashSet<>(List.of("role2")), AuthenticationMethod.LOCAL);
+        assertFalse(workspace.isWriteUser(user));
+    }
+
+    @Test
+    public void test_isWriteUser_ReturnsTrue_WhenTheUserRoleIsAWriteUser() {
+        WorkspaceMetaData workspace = new WorkspaceMetaData(1);
+        workspace.addWriteUser("role1");
+
+        User user = new User("user", new HashSet<>(List.of("role1", "role2")), AuthenticationMethod.LOCAL);
+        assertTrue(workspace.isWriteUser(user));
+    }
+
+    @Test
+    public void test_isLocked_ReturnsFalse_WhenTheWorkspaceIsNotLocked() {
+        WorkspaceMetaData workspaceMetaData = new WorkspaceMetaData(1);
+        workspaceMetaData.setLockedUser(null);
+        workspaceMetaData.setLockedDate(null);
+        assertFalse(workspaceMetaData.isLocked());
+    }
+
+    @Test
+    public void test_isLocked_ReturnsTrue_WhenTheWorkspaceIsLocked() {
+        WorkspaceMetaData workspaceMetaData = new WorkspaceMetaData(1);
+        workspaceMetaData.setLockedUser("simon");
+        workspaceMetaData.setLockedDate(new Date());
+
+        assertTrue(workspaceMetaData.isLocked());
+    }
+
+    @Test
+    public void test_isLocked_ReturnsFalse_WhenTheWorkspaceWasLockedOverFiveMinutesAgo() {
+        WorkspaceMetaData workspaceMetaData = new WorkspaceMetaData(1);
+        workspaceMetaData.setLockedUser("simon");
+        workspaceMetaData.setLockedDate(DateUtils.getXMinutesAgo(6));
+
+        assertFalse(workspaceMetaData.isLocked());
+    }
+
+    @Test
+    public void test_isLockedBy_ReturnsFalse_WhenTheWorkspaceIsNotLocked() {
+        WorkspaceMetaData workspaceMetaData = new WorkspaceMetaData(1);
+        workspaceMetaData.setLockedUser(null);
+        workspaceMetaData.setLockedDate(null);
+        assertFalse(workspaceMetaData.isLockedBy("simon"));
+    }
+
+    @Test
+    public void test_isLockedBy_ReturnsTrue_WhenTheWorkspaceIsLockedByTheSpecifiedUser() {
+        WorkspaceMetaData workspaceMetaData = new WorkspaceMetaData(1);
+        workspaceMetaData.setLockedUser("simon");
+        workspaceMetaData.setLockedDate(new Date());
+
+        assertTrue(workspaceMetaData.isLockedBy("simon"));
+    }
+
+    @Test
+    public void test_isLockedBy_ReturnsFalse_WhenTheWorkspaceIsNotLockedByTheSpecifiedUser() {
+        WorkspaceMetaData workspaceMetaData = new WorkspaceMetaData(1);
+        workspaceMetaData.setLockedUser("simon");
+        workspaceMetaData.setLockedDate(new Date());
+
+        assertFalse(workspaceMetaData.isLockedBy("kirstie"));
+    }
+
+    @Test
+    public void fromProperties_and_toProperties() throws Exception {
+        Properties properties = new Properties();
+        properties.setProperty(WorkspaceMetaData.NAME_PROPERTY, "Name");
+        properties.setProperty(WorkspaceMetaData.DESCRIPTION_PROPERTY, "Description");
+        properties.setProperty(WorkspaceMetaData.VERSION_PROPERTY, "v1.2.3");
+        properties.setProperty(WorkspaceMetaData.CLIENT_SIDE_ENCRYPTED_PROPERTY, "false");
+        properties.setProperty(WorkspaceMetaData.LAST_MODIFIED_USER_PROPERTY, "user1");
+        properties.setProperty(WorkspaceMetaData.LAST_MODIFIED_AGENT_PROPERTY, "structurizr/dsl");
+        properties.setProperty(WorkspaceMetaData.LAST_MODIFIED_DATE_PROPERTY, "2021-01-31T14:30:59Z");
+        properties.setProperty(WorkspaceMetaData.REVISION_PROPERTY, "81");
+        properties.setProperty(WorkspaceMetaData.API_KEY_PROPERTY, "1234567890");
+        properties.setProperty(WorkspaceMetaData.API_SECRET_PROPERTY, "0987654321");
+        properties.setProperty(WorkspaceMetaData.SHARING_TOKEN_PROPERTY, "12345678901234567890");
+        properties.setProperty(WorkspaceMetaData.OWNER_PROPERTY, "user@example.com");
+        properties.setProperty(WorkspaceMetaData.LOCKED_USER_PROPERTY, "user2@example.com");
+        properties.setProperty(WorkspaceMetaData.LOCKED_AGENT_PROPERTY, "structurizr/web");
+        properties.setProperty(WorkspaceMetaData.LOCKED_DATE_PROPERTY, "2022-01-31T14:30:59Z");
+        properties.setProperty(WorkspaceMetaData.READ_USERS_AND_ROLES_PROPERTY, "user1,user2,user3");
+        properties.setProperty(WorkspaceMetaData.WRITE_USERS_AND_ROLES_PROPERTY, "user4,user5,user6");
+
+        WorkspaceMetaData workspace = WorkspaceMetaData.fromProperties(123, properties);
+
+        assertEquals("Name", workspace.getName());
+        assertEquals("Description", workspace.getDescription());
+        assertEquals("v1.2.3", workspace.getVersion());
+        assertFalse(workspace.isClientEncrypted());
+        assertEquals("user1", workspace.getLastModifiedUser());
+        assertEquals("structurizr/dsl", workspace.getLastModifiedAgent());
+        assertEquals(DateUtils.parseIsoDate("2021-01-31T14:30:59Z"), workspace.getLastModifiedDate());
+        assertEquals(81, workspace.getRevision());
+        assertEquals("1234567890", workspace.getApiKey());
+        assertEquals("0987654321", workspace.getApiSecret());
+        assertEquals("12345678901234567890", workspace.getSharingToken());
+        assertEquals("user@example.com", workspace.getOwner());
+        assertEquals("user2@example.com", workspace.getLockedUser());
+        assertEquals("structurizr/web", workspace.getLockedAgent());
+        assertEquals(DateUtils.parseIsoDate("2022-01-31T14:30:59Z"), workspace.getLockedDate());
+        assertEquals(Set.of("user1", "user2", "user3"), workspace.getReadUsers());
+        assertEquals(Set.of("user4", "user5", "user6"), workspace.getWriteUsers());
+        
+        properties.clear();
+        properties = workspace.toProperties();
+
+        assertEquals("Name", properties.getProperty(WorkspaceMetaData.NAME_PROPERTY));
+        assertEquals("Description", properties.getProperty(WorkspaceMetaData.DESCRIPTION_PROPERTY));
+        assertEquals("v1.2.3", properties.getProperty(WorkspaceMetaData.VERSION_PROPERTY));
+        assertEquals("false", properties.getProperty(WorkspaceMetaData.CLIENT_SIDE_ENCRYPTED_PROPERTY));
+        assertEquals("user1", properties.getProperty(WorkspaceMetaData.LAST_MODIFIED_USER_PROPERTY));
+        assertEquals("structurizr/dsl", properties.getProperty(WorkspaceMetaData.LAST_MODIFIED_AGENT_PROPERTY));
+        assertEquals("2021-01-31T14:30:59Z", properties.getProperty(WorkspaceMetaData.LAST_MODIFIED_DATE_PROPERTY));
+        assertEquals("81", properties.getProperty(WorkspaceMetaData.REVISION_PROPERTY));
+        assertEquals("1234567890", properties.getProperty(WorkspaceMetaData.API_KEY_PROPERTY));
+        assertEquals("0987654321", properties.getProperty(WorkspaceMetaData.API_SECRET_PROPERTY));
+        assertEquals("12345678901234567890", properties.getProperty(WorkspaceMetaData.SHARING_TOKEN_PROPERTY));
+        assertEquals("user@example.com", properties.getProperty(WorkspaceMetaData.OWNER_PROPERTY));
+        assertEquals("user2@example.com", properties.getProperty(WorkspaceMetaData.LOCKED_USER_PROPERTY));
+        assertEquals("structurizr/web", properties.getProperty(WorkspaceMetaData.LOCKED_AGENT_PROPERTY));
+        assertEquals("2022-01-31T14:30:59Z", properties.getProperty(WorkspaceMetaData.LOCKED_DATE_PROPERTY));
+        assertEquals("user1,user2,user3", properties.getProperty(WorkspaceMetaData.READ_USERS_AND_ROLES_PROPERTY));
+        assertEquals("user4,user5,user6", properties.getProperty(WorkspaceMetaData.WRITE_USERS_AND_ROLES_PROPERTY));
+    }
+
+}
---
+++ src/main/java/com/structurizr/onpremises/component/workspace/WorkspaceMetaData.java
@@ -0,0 +1,448 @@
+package com.structurizr.onpremises.component.workspace;
+
+import com.structurizr.onpremises.domain.User;
+import com.structurizr.onpremises.domain.UserType;
+import com.structurizr.onpremises.util.DateUtils;
+import com.structurizr.util.StringUtils;
+
+import java.text.ParseException;
+import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.Properties;
+import java.util.Set;
+
+public class WorkspaceMetaData {
+
+    public static final int LOCK_TIMEOUT_IN_MINUTES = 3;
+    
+    static final String NAME_PROPERTY = "name";
+    static final String DESCRIPTION_PROPERTY = "description";
+    static final String VERSION_PROPERTY = "version";
+    static final String CLIENT_SIDE_ENCRYPTED_PROPERTY = "clientSideEncrypted";
+    static final String LAST_MODIFIED_USER_PROPERTY = "lastModifiedUser";
+    static final String LAST_MODIFIED_AGENT_PROPERTY = "lastModifiedAgent";
+    static final String LAST_MODIFIED_DATE_PROPERTY = "lastModifiedDate";
+    static final String REVISION_PROPERTY = "revision";
+    static final String SIZE_PROPERTY = "size";
+    static final String API_KEY_PROPERTY = "apiKey";
+    static final String API_SECRET_PROPERTY = "apiSecret";
+    static final String SHARING_TOKEN_PROPERTY = "sharingToken";
+    static final String OWNER_PROPERTY = "owner";
+    static final String LOCKED_USER_PROPERTY = "lockedUser";
+    static final String LOCKED_AGENT_PROPERTY = "lockedAgent";
+    static final String LOCKED_DATE_PROPERTY = "lockedDate";
+    static final String READ_USERS_AND_ROLES_PROPERTY = "readUsers";
+    static final String WRITE_USERS_AND_ROLES_PROPERTY = "writeUsers";
+
+    private final long id;
+    private String name = "";
+    private String description = "";
+    private String version;
+    private long size;
+    private boolean clientSideEncrypted = false;
+    private String apiKey;
+    private String apiSecret;
+    private String sharingToken = "";
+
+    private Date lastModifiedDate;
+    private String lastModifiedUser;
+    private String lastModifiedAgent;
+
+    private String lockedUser;
+    private String lockedAgent;
+    private Date lockedDate;
+
+    private String internalVersion;
+    private long revision = 0;
+
+    private boolean editable = false;
+
+    private String owner;
+    private final Set<String> readUsers = new LinkedHashSet<>();
+    private final Set<String> writeUsers = new LinkedHashSet<>();
+
+    public WorkspaceMetaData(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public String getApiSecret() {
+        return apiSecret;
+    }
+
+    public void setApiSecret(String apiSecret) {
+        this.apiSecret = apiSecret;
+    }
+
+    public String getSharingToken() {
+        return sharingToken;
+    }
+
+    public void setSharingToken(String sharingToken) {
+        this.sharingToken = sharingToken;
+    }
+
+    public String getSharingTokenTruncated() {
+        return (sharingToken == null ? "" : sharingToken.substring(0, 6)) + "...";
+    }
+
+    public boolean isOpen() {
+        return readUsers.size() == 0 && writeUsers.size() == 0;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getLastModifiedUser() {
+        return lastModifiedUser;
+    }
+
+    public void setLastModifiedUser(String lastModifiedUser) {
+        this.lastModifiedUser = lastModifiedUser;
+    }
+
+    public String getLastModifiedAgent() {
+        return lastModifiedAgent;
+    }
+
+    public void setLastModifiedAgent(String lastModifiedAgent) {
+        this.lastModifiedAgent = lastModifiedAgent;
+    }
+
+    public String getLockedUser() {
+        return lockedUser;
+    }
+
+    public void setLockedUser(String lockedUser) {
+        this.lockedUser = lockedUser;
+    }
+
+    public String getLockedAgent() {
+        return lockedAgent;
+    }
+
+    public void setLockedAgent(String lockedAgent) {
+        this.lockedAgent = lockedAgent;
+    }
+
+    public Date getLockedDate() {
+        return lockedDate;
+    }
+
+    public void setLockedDate(Date lockedDate) {
+        this.lockedDate = lockedDate;
+    }
+
+    public boolean isLocked() {
+        return !StringUtils.isNullOrEmpty(lockedUser) && !DateUtils.isOlderThanXMinutes(lockedDate, LOCK_TIMEOUT_IN_MINUTES);
+    }
+
+    public boolean isLockedBy(String user) {
+        return isLocked() && lockedUser.equals(user);
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public double getSizeInMegaBytes() {
+        return getSize()/(1024.0 * 1024.0);
+    }
+
+    public long getRevision() {
+        return revision;
+    }
+
+    public void setRevision(long revision) {
+        this.revision = revision;
+    }
+
+    public boolean isEditable() {
+        return editable;
+    }
+
+    public void setEditable(boolean editable) {
+        this.editable = editable;
+    }
+
+    public UserType getOwnerUserType() {
+        return new UserType();
+    }
+
+    public boolean isClientEncrypted() {
+        return this.clientSideEncrypted;
+    }
+
+    void setClientSideEncrypted(boolean clientSideEncrypted) {
+        this.clientSideEncrypted = clientSideEncrypted;
+    }
+
+    public boolean isShareable() {
+        return !StringUtils.isNullOrEmpty(sharingToken);
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public Set<String> getReadUsers() {
+        return new LinkedHashSet<>(readUsers);
+    }
+
+    public boolean isReadUser(User user) {
+        if (user == null) {
+            return false;
+        } else {
+            return user.isUserOrRole(readUsers);
+        }
+    }
+
+    public void addReadUser(String user) {
+        if (user != null) {
+            user = user.trim();
+            if (user.length() > 0) {
+                readUsers.add(user.toLowerCase());
+            }
+        }
+    }
+
+    public void clearReadUsers() {
+        readUsers.clear();
+    }
+
+    public Set<String> getWriteUsers() {
+        return new LinkedHashSet<>(writeUsers);
+    }
+
+    public boolean isWriteUser(User user) {
+        if (user == null) {
+            return false;
+        } else {
+            return user.isUserOrRole(writeUsers);
+        }
+    }
+
+    public void addWriteUser(String user) {
+        if (user != null) {
+            user = user.trim();
+            if (user.length() > 0) {
+                writeUsers.add(user.toLowerCase());
+            }
+        }
+    }
+
+    public void clearWriteUsers() {
+        writeUsers.clear();
+    }
+
+    public boolean isOwner(User user) {
+        return StringUtils.isNullOrEmpty(owner) || owner.equals(user.getUsername());
+    }
+
+    public String getInternalVersion() {
+        return internalVersion;
+    }
+
+    public void setInternalVersion(String internalVersion) {
+        this.internalVersion = internalVersion;
+    }
+
+    public boolean isActive() {
+        return true;
+    }
+
+    public void addLock(String username, String agent) {
+        this.lockedUser = username;
+        this.lockedAgent = agent;
+        this.lockedDate = new Date();
+    }
+    
+    public void clearLock() {
+        this.lockedUser = null;
+        this.lockedAgent = null;
+        this.lockedDate = null;
+    }
+
+    public static WorkspaceMetaData fromProperties(long workspaceId, Properties properties) {
+        WorkspaceMetaData workspace = new WorkspaceMetaData(workspaceId);
+        workspace.setName(properties.getProperty(NAME_PROPERTY));
+        workspace.setDescription(properties.getProperty(DESCRIPTION_PROPERTY));
+        workspace.setVersion(properties.getProperty(VERSION_PROPERTY));
+        workspace.setClientSideEncrypted("true".equals(properties.getProperty(CLIENT_SIDE_ENCRYPTED_PROPERTY)));
+        workspace.setLastModifiedUser(properties.getProperty(LAST_MODIFIED_USER_PROPERTY));
+        workspace.setLastModifiedAgent(properties.getProperty(LAST_MODIFIED_AGENT_PROPERTY));
+        try {
+            workspace.setLastModifiedDate(DateUtils.parseIsoDate(properties.getProperty(LAST_MODIFIED_DATE_PROPERTY)));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        workspace.setRevision(Long.parseLong(properties.getProperty(REVISION_PROPERTY, "0")));
+        workspace.setSize(Long.parseLong(properties.getProperty(SIZE_PROPERTY, "0")));
+        workspace.setApiKey(properties.getProperty(API_KEY_PROPERTY, ""));
+        workspace.setApiSecret(properties.getProperty(API_SECRET_PROPERTY, ""));
+        workspace.setSharingToken(properties.getProperty(SHARING_TOKEN_PROPERTY, ""));
+        workspace.setOwner(properties.getProperty(OWNER_PROPERTY, ""));
+
+        workspace.setLockedUser(properties.getProperty(LOCKED_USER_PROPERTY, null));
+        workspace.setLockedAgent(properties.getProperty(LOCKED_AGENT_PROPERTY, null));
+        try {
+            workspace.setLockedDate(DateUtils.parseIsoDate(properties.getProperty(LOCKED_DATE_PROPERTY)));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        String users = properties.getProperty(READ_USERS_AND_ROLES_PROPERTY);
+        if (users != null) {
+            String[] usersAsArray = users.split(",");
+            for (String user : usersAsArray) {
+                workspace.addReadUser(user);
+            }
+        }
+
+        users = properties.getProperty(WRITE_USERS_AND_ROLES_PROPERTY);
+        if (users != null) {
+            String[] usersAsArray = users.split(",");
+            for (String user : usersAsArray) {
+                workspace.addWriteUser(user);
+            }
+        }
+
+        return workspace;
+    }
+
+    public Properties toProperties() {
+        Properties properties = new Properties();
+
+        if (this.getName() != null) {
+            properties.setProperty(NAME_PROPERTY, this.getName());
+        } else {
+            properties.setProperty(NAME_PROPERTY, "");
+        }
+
+        if (this.getDescription() != null) {
+            properties.setProperty(DESCRIPTION_PROPERTY, this.getDescription());
+        } else {
+            properties.setProperty(DESCRIPTION_PROPERTY, "");
+        }
+
+        properties.setProperty(VERSION_PROPERTY, this.getVersion());
+
+        properties.setProperty(CLIENT_SIDE_ENCRYPTED_PROPERTY, "" + this.isClientEncrypted());
+
+        if (this.getLastModifiedUser() != null) {
+            properties.setProperty(LAST_MODIFIED_USER_PROPERTY, this.getLastModifiedUser());
+        } else {
+            properties.setProperty(LAST_MODIFIED_USER_PROPERTY, "");
+        }
+
+        if (this.getLastModifiedAgent() != null) {
+            properties.setProperty(LAST_MODIFIED_AGENT_PROPERTY, this.getLastModifiedAgent());
+        } else {
+            properties.setProperty(LAST_MODIFIED_AGENT_PROPERTY, "");
+        }
+
+        if (this.getLastModifiedDate() != null) {
+            properties.setProperty(LAST_MODIFIED_DATE_PROPERTY, DateUtils.formatIsoDate(this.getLastModifiedDate()));
+        } else {
+            properties.setProperty(LAST_MODIFIED_DATE_PROPERTY, "");
+        }
+
+        properties.setProperty(REVISION_PROPERTY, "" + this.getRevision());
+
+        if (!StringUtils.isNullOrEmpty(this.getOwner())) {
+            properties.setProperty(OWNER_PROPERTY, this.getOwner());
+        }
+        properties.setProperty(READ_USERS_AND_ROLES_PROPERTY, toCommaSeparatedString(this.getReadUsers()));
+        properties.setProperty(WRITE_USERS_AND_ROLES_PROPERTY, toCommaSeparatedString(this.getWriteUsers()));
+
+        properties.setProperty(SIZE_PROPERTY, "" + this.getSize());
+
+        properties.setProperty(API_KEY_PROPERTY, this.getApiKey());
+        properties.setProperty(API_SECRET_PROPERTY, this.getApiSecret());
+
+        if (this.getSharingToken() != null) {
+            properties.setProperty(SHARING_TOKEN_PROPERTY, this.getSharingToken());
+        }
+
+        if (this.getLockedUser() != null) {
+            properties.setProperty(LOCKED_USER_PROPERTY, this.getLockedUser());
+        }
+
+        if (this.getLockedAgent() != null) {
+            properties.setProperty(LOCKED_AGENT_PROPERTY, this.getLockedAgent());
+        }
+
+        if (this.getLockedDate() != null) {
+            properties.setProperty(LOCKED_DATE_PROPERTY, DateUtils.formatIsoDate(this.getLockedDate()));
+        } else {
+            properties.setProperty(LOCKED_DATE_PROPERTY, "");
+        }
+
+        return properties;
+    }
+
+    private String toCommaSeparatedString(Set<String> strings) {
+        StringBuilder buf = new StringBuilder();
+        if (strings != null) {
+            for (String username : strings) {
+                buf.append(username);
+                buf.append(",");
+            }
+        }
+
+        if (buf.toString().endsWith(",")) {
+            return buf.substring(0, buf.length()-1);
+        } else {
+            return buf.toString();
+        }
+    }
+
+}
