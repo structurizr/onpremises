83fe4974c7e8ec4f8b7c178f3f630b8e9cbe4463
Simon Brown <simon@structurizr.com>
2023-02-05 23:48:00 +0000

    .

---
+++ src/test/java/com/structurizr/onpremises/web/MockSearchComponent.java
@@ -0,0 +1,38 @@
+package com.structurizr.onpremises.web;
+
+import com.structurizr.Workspace;
+import com.structurizr.onpremises.component.search.SearchComponent;
+import com.structurizr.onpremises.component.search.SearchResult;
+
+import java.util.List;
+import java.util.Set;
+
+public class MockSearchComponent implements SearchComponent {
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void stop() {
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public void index(Workspace workspace) {
+    }
+
+    @Override
+    public List<SearchResult> search(String query, String type, Set<Long> workspaceIds) {
+        return null;
+    }
+
+    @Override
+    public void delete(long workspaceId) {
+    }
+
+}
