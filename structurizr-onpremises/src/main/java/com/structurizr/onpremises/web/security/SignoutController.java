5c550323c7469aaed6cc0e7e4fad42c133308f85
Simon Brown <simon@structurizr.com>
2023-12-20 11:24:01 +0000

    Sign out works now.

--- structurizr-onpremises/src/main/java/com/structurizr/onpremises/web/security/SignoutController.java
+++ structurizr-onpremises/src/main/java/com/structurizr/onpremises/web/security/SignoutController.java
@@ -3,6 +3,9 @@ package com.structurizr.onpremises.web.security;
 import com.structurizr.onpremises.domain.AuthenticationMethod;
 import com.structurizr.onpremises.domain.User;
 import com.structurizr.onpremises.web.AbstractController;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,21 +18,25 @@ import jakarta.servlet.http.HttpServletResponse;
 public class SignoutController extends AbstractController {
 
     @RequestMapping(value = "/signout", method = RequestMethod.GET)
-   	public String signout(HttpServletResponse response) {
+    @PreAuthorize("isAuthenticated()")
+   	public String signout(HttpServletRequest request, HttpServletResponse response) {
+        //SecurityContextHolder.getContext().setAuthentication(null);
+
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+
         User user = getUser();
-
-        if (user.getAuthenticationMethod() == AuthenticationMethod.SAML) {
-            return "redirect:/saml/logout?local=true";
-        } else {
-            SecurityContextHolder.getContext().setAuthentication(null);
-
+        if (user.getAuthenticationMethod() == AuthenticationMethod.LOCAL) {
             Cookie cookie = new Cookie("remember-me", null);
             cookie.setMaxAge(0);
 
             response.addCookie(cookie);
 
-            return "redirect:/";
         }
+
+        return "redirect:/";
    	}
 
 }
