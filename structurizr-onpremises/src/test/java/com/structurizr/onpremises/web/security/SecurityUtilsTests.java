085cb4a44bda81b5ad5d6588cbdf10f6a67cc303
Simon Brown <simon@structurizr.com>
2023-12-29 16:16:17 +0000

    Reinstate SAML principal test.

--- structurizr-onpremises/src/test/java/com/structurizr/onpremises/web/security/SecurityUtilsTests.java
+++ structurizr-onpremises/src/test/java/com/structurizr/onpremises/web/security/SecurityUtilsTests.java
@@ -2,16 +2,19 @@ package com.structurizr.onpremises.web.security;
 
 import com.structurizr.onpremises.domain.Role;
 import com.structurizr.onpremises.domain.User;
+import com.structurizr.onpremises.util.Configuration;
+import com.structurizr.onpremises.util.StructurizrProperties;
 import org.junit.jupiter.api.Test;
-//import org.opensaml.saml2.core.impl.AssertionBuilder;
-//import org.opensaml.saml2.core.impl.NameIDBuilder;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.saml.SAMLCredential;
+import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticatedPrincipal;
+import org.springframework.security.saml2.provider.service.authentication.Saml2Authentication;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -35,68 +38,46 @@ public class SecurityUtilsTests {
         assertTrue(user.getRoles().contains("role2"));
    }
 
-//    @Test
-//    public void getUser_ViaSAMLWithGroups() {
-//        SAMLCredential credential = new SAMLCredential(
-//                new NameIDBuilder().buildObject(), new AssertionBuilder().buildObject(), null, null) {
-//            @Override
-//            public String getAttributeAsString(String name) {
-//                if (name.equals(SecurityUtils.SAML_EMAIL_ADDRESS_ATTRIBUTE)) {
-//                    return "user";
-//                } else {
-//                    return "";
-//                }
-//            }
-//
-//            @Override
-//            public String[] getAttributeAsStringArray(String name) {
-//                if (name.equals(SecurityUtils.SAML_GROUP_ATTRIBUTE)) {
-//                    return new String[] { "role1", "role2" };
-//                } else {
-//                    return new String[]{};
-//                }
-//            }
-//        };
-//        Authentication authentication = new UsernamePasswordAuthenticationToken("structurizr", credential, new HashSet<Role>());
-//        SecurityContextHolder.getContext().setAuthentication(authentication);
-//
-//        User user = SecurityUtils.getUser();
-//        assertEquals("user", user.getUsername());
-//        assertEquals(2, user.getRoles().size());
-//        assertTrue(user.getRoles().contains("role1"));
-//        assertTrue(user.getRoles().contains("role2"));
-//    }
-//
-//    @Test
-//    public void getUser_ViaSAMLWithAuth0Roles() {
-//        SAMLCredential credential = new SAMLCredential(
-//                new NameIDBuilder().buildObject(), new AssertionBuilder().buildObject(), null, null) {
-//            @Override
-//            public String getAttributeAsString(String name) {
-//                if (name.equals(SecurityUtils.SAML_EMAIL_ADDRESS_ATTRIBUTE)) {
-//                    return "user";
-//                } else {
-//                    return "";
-//                }
-//            }
-//
-//            @Override
-//            public String[] getAttributeAsStringArray(String name) {
-//                if (name.equals(SecurityUtils.SAML_AUTH0_ROLES_ATTRIBUTE)) {
-//                    return new String[] { "role1", "role2" };
-//                } else {
-//                    return new String[]{};
-//                }
-//            }
-//        };
-//        Authentication authentication = new UsernamePasswordAuthenticationToken("structurizr", credential, new HashSet<Role>());
-//        SecurityContextHolder.getContext().setAuthentication(authentication);
-//
-//        User user = SecurityUtils.getUser();
-//        assertEquals("user", user.getUsername());
-//        assertEquals(2, user.getRoles().size());
-//        assertTrue(user.getRoles().contains("role1"));
-//        assertTrue(user.getRoles().contains("role2"));
-//    }
+    @Test
+    public void getUser_ViaSAMLWithGroups() {
+        Saml2AuthenticatedPrincipal principal = new Saml2AuthenticatedPrincipal() {
+            @Override
+            public String getName() {
+                return null;
+            }
+
+            @Override
+            public String getFirstAttribute(String name) {
+                if (StructurizrProperties.DEFAULT_SAML_ATTRIBUTE_USERNAME.equals(name)) {
+                    return "user";
+                } else {
+                    return null;
+                }
+            }
+
+            @Override
+            public List<Object> getAttribute(String name) {
+                if (StructurizrProperties.DEFAULT_SAML_ATTRIBUTE_ROLE.equals(name)) {
+                    List<Object> list = new ArrayList<>();
+                    list.add("role1");
+                    list.add("role2");
+                    return list;
+                } else {
+                    return null;
+                }
+            }
+        };
+
+        Saml2Authentication credential = new Saml2Authentication(principal, "SAML response", null);
+        //Authentication authentication = new UsernamePasswordAuthenticationToken("structurizr", credential, new HashSet<Role>());
+        SecurityContextHolder.getContext().setAuthentication(credential);
+
+        Configuration.init();
+        User user = SecurityUtils.getUser();
+        assertEquals("user", user.getUsername());
+        assertEquals(2, user.getRoles().size());
+        assertTrue(user.getRoles().contains("role1"));
+        assertTrue(user.getRoles().contains("role2"));
+    }
 
 }
