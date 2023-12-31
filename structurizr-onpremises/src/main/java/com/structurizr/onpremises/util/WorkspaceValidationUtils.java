de1b41c242ee780b4135ca1d27d1bd0d29bafa7c
Simon Brown <simon@structurizr.com>
2023-12-31 10:45:22 +0000

    Tweaks error message.

--- structurizr-onpremises/src/main/java/com/structurizr/onpremises/util/WorkspaceValidationUtils.java
+++ structurizr-onpremises/src/main/java/com/structurizr/onpremises/util/WorkspaceValidationUtils.java
@@ -10,7 +10,7 @@ public class WorkspaceValidationUtils {
         // if workspace scope validation is enabled, reject workspaces without a defined scope
         if (Configuration.getInstance().isFeatureEnabled(Features.WORKSPACE_SCOPE_VALIDATION)) {
             if (workspace.getConfiguration().getScope() == null) {
-                throw new WorkspaceScopeValidationException("Strict workspace scope validation has been enabled on this on-premises installation, but this workspace has no defined scope - see https://docs.structurizr.com/workspaces for more information.");
+                throw new WorkspaceScopeValidationException("Strict workspace scope validation has been enabled for this on-premises installation. Unscoped workspaces are not permitted - see https://docs.structurizr.com/workspaces for more information.");
             }
         }
 
