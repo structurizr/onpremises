e911702d12d93d4f9865b8a36c54de68c375e9ad
Simon Brown <simon@structurizr.com>
2024-06-30 16:01:01 +0100

    Adds the new model explorer.

---
+++ structurizr-onpremises/src/main/java/com/structurizr/onpremises/web/workspace/ModelController.java
@@ -0,0 +1,60 @@
+package com.structurizr.onpremises.web.workspace;
+
+import com.structurizr.onpremises.component.workspace.WorkspaceMetaData;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ModelController extends AbstractWorkspaceController {
+
+    private static final String VIEW = "model";
+
+    @RequestMapping(value = "/share/{workspaceId}/explore/model", method = RequestMethod.GET)
+    public String showPublicModel(
+            @PathVariable("workspaceId") long workspaceId,
+            @RequestParam(required = false) String version,
+            @RequestParam(required = false) String view,
+            ModelMap model
+    ) {
+        model.addAttribute("view", view);
+
+        return showPublicView(VIEW, workspaceId, version, model, true);
+    }
+
+    @RequestMapping(value = "/share/{workspaceId}/{token}/explore/model", method = RequestMethod.GET)
+    public String showSharedModel(
+            @PathVariable("workspaceId") long workspaceId,
+            @RequestParam(required = false) String version,
+            @RequestParam(required = false) String view,
+            @PathVariable("token") String token,
+            ModelMap model
+    ) {
+        model.addAttribute("view", view);
+
+        return showSharedView(VIEW, workspaceId, token, version, model, true);
+    }
+
+    @RequestMapping(value = "/workspace/{workspaceId}/explore/model", method = RequestMethod.GET)
+    @PreAuthorize("isAuthenticated()")
+    public String showAuthenticatedModel(
+            @PathVariable("workspaceId") long workspaceId,
+            @RequestParam(required = false) String version,
+            @RequestParam(required = false) String view,
+            ModelMap model
+    ) {
+        WorkspaceMetaData workspaceMetaData = workspaceComponent.getWorkspaceMetaData(workspaceId);
+        if (workspaceMetaData == null) {
+            return show404Page(model);
+        }
+
+        model.addAttribute("view", view);
+
+        return showAuthenticatedView(VIEW, workspaceMetaData, version, model, true, false);
+    }
+
+}
