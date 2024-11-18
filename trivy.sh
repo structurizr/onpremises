85dc18a515ad1f0b2ba9c6962568ba24f0ac1e90
Simon Brown <simon@structurizr.com>
2024-11-18 12:29:44 +0000

    Use different repo.

--- trivy.sh
+++ trivy.sh
@@ -1,1 +1,2 @@
-docker run aquasec/trivy image structurizr/onpremises
+#docker run aquasec/trivy image structurizr/onpremises
+docker run -e "TRIVY_DB_REPOSITORY=public.ecr.aws/aquasecurity/trivy-db" -e "TRIVY_JAVA_DB_REPOSITORY=public.ecr.aws/aquasecurity/trivy-java-db" aquasec/trivy image structurizr/onpremises
