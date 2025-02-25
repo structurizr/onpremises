f37902d74873649b3b4338a93facc3d02872fce7
Simon Brown <simon@structurizr.com>
2025-02-25 11:29:00 +0000

    Fixes broken link.

--- structurizr-onpremises/src/main/java/com/structurizr/onpremises/util/EarlyAccessFeaturesNotAvailableException.java
+++ structurizr-onpremises/src/main/java/com/structurizr/onpremises/util/EarlyAccessFeaturesNotAvailableException.java
@@ -3,7 +3,7 @@ package com.structurizr.onpremises.util;
 public final class EarlyAccessFeaturesNotAvailableException extends RuntimeException {
 
     public EarlyAccessFeaturesNotAvailableException(String feature) {
-        super(feature + " is not available in this build - see https://docs.structurizr.com/onpremises/early-access for details of how to gain early access to new features");
+        super(feature + " is not available in this build - see https://docs.structurizr.com/onpremises for details of how to gain early access to new features");
     }
 
 }
